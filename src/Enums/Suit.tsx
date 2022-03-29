46223596330721c3c59bbb985a4922a5b8f6d358
Lucius Bachmann <lucius.bachmann@scs.ch>
2022-03-29 12:03:08 +0200

    lint: fix not auto fixable problems
    Remove unused imports.
    Remove unused variables.
    Disable inspection for unused vars (temporarily).

--- src/Enums/Suit.tsx
+++ src/Enums/Suit.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 enum Suit {
   Schellen = 1,
   Rosen = 2,
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 
 const Button = (props) => {
   return <h1>Button ui</h1>
