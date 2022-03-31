1d4d17fef884cd6d115a91311bc5c3346ec601ae
CarmenUZH <91155317+CarmenUZH@users.noreply.github.com>
2022-03-31 18:32:00 +0200

    Changed App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import React from "react"
 
 test("renders register page", () => {
   render(<App />)
-  const linkElement = screen.getByText(/Register page/i)
+  const linkElement = screen.getByText(/Welcome to "Screw your neighbour"/i)
   expect(linkElement).toBeInTheDocument()
 })
