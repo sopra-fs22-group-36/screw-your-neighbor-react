8cad08726dcde9e800689b7b862e45ac8753dc70
BacLuc <lucius.bachmann@gmx.ch>
2022-04-20 00:38:20 +0200

    VideoChat: add simple integration of jitsi meet
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/123

--- src/components/views/game/game/videochat/VideoChat.tsx
+++ src/components/views/game/game/videochat/VideoChat.tsx
@@ -1,11 +1,71 @@
-import React from "react"
-
+import React, { useState } from "react"
+import { observer } from "mobx-react-lite"
+import { usePlayers } from "../../../../../hooks/api/usePlayers"
+import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
+import Button from "@mui/material/Button"
+import { VideoCameraFront } from "@mui/icons-material"
+import { JitsiMeeting } from "@jitsi/react-sdk"
 import "./VideoChat.scss"
 
-export const VideoChat = () => {
+const jitsiConfig = {
+  disableModeratorIndicator: true,
+  disablePolls: true,
+  disableSelfView: true,
+  disableSelfViewSettings: true,
+  hideLobbyButton: true,
+  requireDisplayName: false,
+  hideDisplayName: true,
+  hideDominantSpeakerBadge: true,
+  disableProfile: true,
+  prejoinConfig: {
+    enabled: false,
+  },
+  readOnlyName: true,
+  toolbarButtons: [],
+}
+export const VideoChat = observer(() => {
+  const { me } = usePlayers()
+  const { game } = useCurrentGame()
+
+  const [active, setActive] = useState(false)
+
+  const additionalClass = active ? "active" : "inactive"
+
   return (
-    <div className={"videochat"}>
-      <h3 className={"text"}>Here comes the video chat</h3>
+    <div className={`videochat ${additionalClass}`}>
+      {(() => {
+        if (!active) {
+          return (
+            <Button
+              className={"toggle-camera"}
+              variant="contained"
+              endIcon={<VideoCameraFront />}
+              onClick={() => setActive(true)}
+            >
+              Activate video chat
+            </Button>
+          )
+        }
+        return (
+          <div className={"video-container"}>
+            <JitsiMeeting
+              roomName={game.videoChatName}
+              configOverwrite={jitsiConfig}
+              userInfo={{
+                displayName: me.name,
+                email: "",
+              }}
+              interfaceConfigOverwrite={{
+                filmStripOnly: true,
+              }}
+              getIFrameRef={(iframe) => {
+                // does not work how i want it. But you can change the style of the container div
+                iframe.style.objectFit = "fill"
+              }}
+            />
+          </div>
+        )
+      })()}
     </div>
   )
-}
+})
