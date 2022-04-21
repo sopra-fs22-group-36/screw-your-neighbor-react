0267c8864a66d15f404af903a104e7c48f55dac9
BacLuc <lucius.bachmann@gmx.ch>
2022-04-21 17:57:38 +0200

    ScoreBoard: start with styling
    Issue: #68

--- src/components/views/game/game/scoreboard/ScoreBoard.tsx
+++ src/components/views/game/game/scoreboard/ScoreBoard.tsx
@@ -1,11 +1,29 @@
 import React from "react"
+import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
 
+import BaseContainer from "../../../../ui/BaseContainer"
 import "./ScoreBoard.scss"
 
 export const ScoreBoard = () => {
+  const { activeParticipations } = useCurrentGame()
+
   return (
     <div className={"scoreboard"}>
-      <h3 className={"text"}>Here comes the scoreboard</h3>
+      <BaseContainer>
+        <h1>ScoreBoard</h1>
+        <ul className={"player-list"}>
+          {activeParticipations.map((participation) => {
+            return (
+              <li key={participation._links.self.href}>
+                <span className={"player-name"}>
+                  {participation.player.name}
+                </span>
+                <span className={"points"}>Points: {participation.points}</span>
+              </li>
+            )
+          })}
+        </ul>
+      </BaseContainer>
     </div>
   )
 }
