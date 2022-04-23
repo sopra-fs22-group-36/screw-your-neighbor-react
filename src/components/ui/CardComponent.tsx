5ca1d24fc25114a1cab30dd7a041b84fb2a347a2
elBeato <beatus.furrer@gmail.com>
2022-04-23 18:21:20 +0200

    all cards as png

--- src/components/ui/CardComponent.tsx
+++ src/components/ui/CardComponent.tsx
@@ -1,10 +1,26 @@
+import { number } from "prop-types"
 import React, { MouseEventHandler } from "react"
 import { Card } from "../../generated"
 import "./CardComponent.scss"
+import eichelAss from "../../img/deck/eichelAss.png"
 
 export type CardComponentProps = {
   card: Card
   onClick?: MouseEventHandler<HTMLDivElement>
+}
+
+function findCard (rank: Card.cardRank, suit: Card.cardSuit): string {
+  let cardName = ""
+  if (Card.cardSuit.CLUB === suit)
+    cardName = "schellen"
+  if (Card.cardSuit.DIAMOND === suit)
+    cardName = "schilten"
+  if (Card.cardSuit.HEART === suit)
+    cardName = "rosen"
+  if (Card.cardSuit.SPADE === suit)
+    cardName = "eichel"
+  cardName.concat(rank.toString(), ".png")
+  return cardName
 }
 
 export const CardComponent = (props: CardComponentProps) => {
@@ -12,6 +28,7 @@ export const CardComponent = (props: CardComponentProps) => {
   return (
     <div className="card-component" onClick={onClick}>
       <div>
+        <img src={eichelAss} />
         {props.card.cardRank} of {props.card.cardSuit}
       </div>
     </div>
