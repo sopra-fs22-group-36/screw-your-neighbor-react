ce8a20bd684c8fdd915b6be4cb37d78e46bffe17
BacLuc <lucius.bachmann@gmx.ch>
2022-04-27 23:11:49 +0200

    ScoreBoard.tsx: show own name with different color
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/58

--- src/components/views/game/game/scoreboard/ScoreBoard.tsx
+++ src/components/views/game/game/scoreboard/ScoreBoard.tsx
@@ -7,7 +7,8 @@ import { Chip } from "@mui/material"
 import "./ScoreBoard.scss"
 
 export const ScoreBoard = observer(() => {
-  const { activeParticipations, activeMatch } = useCurrentGame()
+  const { activeParticipations, activeMatch, myParticipation } =
+    useCurrentGame()
 
   return (
     <div className={"scoreboard"}>
@@ -15,12 +16,23 @@ export const ScoreBoard = observer(() => {
         <h1>ScoreBoard</h1>
         <ul className={"player-list"}>
           {activeParticipations.map((participation) => {
-            const self = participation._links.self
+            const participationLink = participation._links.self
             const turnActive = activeMatch.hands
-              .filter((hand) => iriMatch(hand.participation._links.self, self))
+              .filter((hand) =>
+                iriMatch(hand.participation._links.self, participationLink)
+              )
               .some((hand) => hand.turnActive)
+
+            const isOwnParticipation = iriMatch(
+              participationLink,
+              myParticipation._links.self
+            )
+
             return (
-              <li key={self.href}>
+              <li
+                key={participationLink.href}
+                className={isOwnParticipation && "own-participation"}
+              >
                 <span className={"player-name"}>
                   {participation.player.name}
                 </span>
