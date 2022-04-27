621b9d9cda49aa0498e2bdce26aa3bdb11c09ec6
BacLuc <lucius.bachmann@gmx.ch>
2022-04-27 23:06:13 +0200

    ScoreBoard.tsx: add chip to show who should play
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/58

--- src/components/views/game/game/scoreboard/ScoreBoard.tsx
+++ src/components/views/game/game/scoreboard/ScoreBoard.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
+import { iriMatch } from "../../../../../util/iriMatch"
 import BaseContainer from "../../../../ui/BaseContainer"
+import { Chip } from "@mui/material"
 import "./ScoreBoard.scss"
 
 export const ScoreBoard = observer(() => {
-  const { activeParticipations } = useCurrentGame()
+  const { activeParticipations, activeMatch } = useCurrentGame()
 
   return (
     <div className={"scoreboard"}>
@@ -13,11 +15,24 @@ export const ScoreBoard = observer(() => {
         <h1>ScoreBoard</h1>
         <ul className={"player-list"}>
           {activeParticipations.map((participation) => {
+            const self = participation._links.self
+            const turnActive = activeMatch.hands
+              .filter((hand) => iriMatch(hand.participation._links.self, self))
+              .some((hand) => hand.turnActive)
             return (
-              <li key={participation._links.self.href}>
+              <li key={self.href}>
                 <span className={"player-name"}>
                   {participation.player.name}
                 </span>
+                {turnActive && (
+                  <Chip
+                    color={"info"}
+                    label={"Playing"}
+                    className={"chip"}
+                    size={"small"}
+                  ></Chip>
+                )}
+
                 <span className={"points"}>Points: {participation.points}</span>
               </li>
             )
