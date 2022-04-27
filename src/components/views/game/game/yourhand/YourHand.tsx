2ecfe2e07011f059b56e543b0fa8e39896eb3ad2
CarmenUZH <91155317+CarmenUZH@users.noreply.github.com>
2022-04-27 17:41:06 +0200

    Finally removed "setOpacity"

--- src/components/views/game/game/yourhand/YourHand.tsx
+++ src/components/views/game/game/yourhand/YourHand.tsx
@@ -14,15 +14,13 @@ import "./YourHand.scss"
 export const YourHand = () => {
   const { updatecards } = useCards()
   const { activeMatch, yourActiveHand } = useCurrentGame()
-  const [opacity, setOpacity] = useState(10)
   const [open, setOpen] = useState(false)
 
   const notYetPlayed =
     yourActiveHand?.cards.filter((value) => value.round === null) ?? []
 
   //Check if all players did the score announcement for this match
-  const clickCard = async (card, number) => {
-    setOpacity(number)
+  const clickCard = async (card) => {
     if (activeMatch.matchState === Match.matchState.PLAYING) {
       updatecards(card)
     } else {
@@ -68,13 +66,8 @@ export const YourHand = () => {
     <div className="your-hand">
       <p>These are the Cards for Match {activeMatch?.matchNumber}:</p>
       {notYetPlayed.map((card, index) => (
-        <div
-          style={{
-            backgroundColor: opacity === index ? "#0b97c4" : "#006666",
-          }}
-          key={card._links.self.href}
-        >
-          <CardComponent card={card} onClick={() => clickCard(card, index)} />
+        <div key={card._links.self.href}>
+          <CardComponent card={card} onClick={() => clickCard(card)} />
         </div>
       ))}
       {content}
