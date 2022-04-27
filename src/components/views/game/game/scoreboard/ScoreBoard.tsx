7abae6a16a2b8141aac37482377d9e8a5f97441c
BacLuc <lucius.bachmann@gmx.ch>
2022-04-27 23:32:13 +0200

    refactor/ScoreBoard.tsx: extract PlayerRow
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/58

--- src/components/views/game/game/scoreboard/ScoreBoard.tsx
+++ src/components/views/game/game/scoreboard/ScoreBoard.tsx
@@ -1,14 +1,49 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
+import { Participation } from "../../../../../generated"
 import { iriMatch } from "../../../../../util/iriMatch"
 import BaseContainer from "../../../../ui/BaseContainer"
 import { Chip } from "@mui/material"
 import "./ScoreBoard.scss"
 
+type PlayerRowProps = {
+  participation: Participation
+}
+
+const PlayerRow = observer((props: PlayerRowProps) => {
+  const { participation } = props
+  const { activeMatch, myParticipation } = useCurrentGame()
+  const participationLink = participation._links.self
+  const turnActive = activeMatch.hands
+    .filter((hand) =>
+      iriMatch(hand.participation._links.self, participationLink)
+    )
+    .some((hand) => hand.turnActive)
+
+  const isOwnParticipation = iriMatch(
+    participationLink,
+    myParticipation._links.self
+  )
+  return (
+    <li className={isOwnParticipation && "own-participation"}>
+      <span className={"player-name"}>{participation.player.name}</span>
+      {turnActive && (
+        <Chip
+          color={"info"}
+          label={"Playing"}
+          className={"chip"}
+          size={"small"}
+        ></Chip>
+      )}
+
+      <span className={"points"}>Points: {participation.points}</span>
+    </li>
+  )
+})
+
 export const ScoreBoard = observer(() => {
-  const { activeParticipations, activeMatch, myParticipation } =
-    useCurrentGame()
+  const { activeParticipations } = useCurrentGame()
 
   return (
     <div className={"scoreboard"}>
@@ -16,37 +51,11 @@ export const ScoreBoard = observer(() => {
         <h1>ScoreBoard</h1>
         <ul className={"player-list"}>
           {activeParticipations.map((participation) => {
-            const participationLink = participation._links.self
-            const turnActive = activeMatch.hands
-              .filter((hand) =>
-                iriMatch(hand.participation._links.self, participationLink)
-              )
-              .some((hand) => hand.turnActive)
-
-            const isOwnParticipation = iriMatch(
-              participationLink,
-              myParticipation._links.self
-            )
-
             return (
-              <li
-                key={participationLink.href}
-                className={isOwnParticipation && "own-participation"}
-              >
-                <span className={"player-name"}>
-                  {participation.player.name}
-                </span>
-                {turnActive && (
-                  <Chip
-                    color={"info"}
-                    label={"Playing"}
-                    className={"chip"}
-                    size={"small"}
-                  ></Chip>
-                )}
-
-                <span className={"points"}>Points: {participation.points}</span>
-              </li>
+              <PlayerRow
+                key={participation._links.self.href}
+                participation={participation}
+              />
             )
           })}
         </ul>
