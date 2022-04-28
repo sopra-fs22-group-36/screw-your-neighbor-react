7dec77d421cca2e8ebbd324e122ae873fbad23c2
CarmenUZH <91155317+CarmenUZH@users.noreply.github.com>
2022-04-28 19:03:11 +0200

    Hamburger Animations
    Also: Code looks cleaner

--- src/components/ui/HamburgerMenu.tsx
+++ src/components/ui/HamburgerMenu.tsx
@@ -7,55 +7,54 @@ import ListAltIcon from "@mui/icons-material/ListAlt"
 import { useCurrentGame } from "../../hooks/api/useCurrentGame"
 import { Paths } from "../routing/routers/Paths"
 import { useNavigate } from "react-router-dom"
+import { Grow } from "@mui/material"
 
 const HamburgerMenu = () => {
   const navigate = useNavigate()
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
-  const [showRules, setShowRules] = useState(" ")
+  const [showRules, setShowRules] = useState(false)
   const { leaveGame } = useCurrentGame()
-  const [marginLeave, setMarginLeave] = useState("700px")
+  const theRules = (
+    <p>
+      {" "}
+      "One game is split up into nine matches where you first get decreasing
+      numbers of cards each match, with a minimum of one card, and then
+      increasing numbers of cards again. (5,4,3,2,1,2,3,4) At the beginning of
+      each match, you announce a number of “Tricks”. “Tricks” are the number of
+      rounds won for each match. During each round, each player places a card
+      from their hand. You win a round if your card is the one with the highest
+      value. The goal of the game is to announce a certain number of tricks and
+      then actually winning them as well.Winning the announced amount of tricks
+      will yield you points, differentiating from your announcement you will get
+      points deducted. The player with the most points at the end has won.
+      During the special round where you only have one card, you won’t be able
+      to see your card until after everyone has announced their tricks, but you
+      may see the cards of all the other players. Have Fun! :)
+    </p>
+  )
 
   const clickLeave = async () => {
     await leaveGame()
     navigate(Paths.LOBBY)
   }
   const clickRules = async () => {
-    setMarginLeave("86px")
-    setShowRules(
-      "One game is split up into nine matches where you first get decreasing numbers of cards each match,\n" +
-        "with a minimum of one card, \n" +
-        "and then increasing numbers of cards again. (5,4,3,2,1,2,3,4)\n" +
-        "At the beginning of each match, you announce a number of “Tricks”. \n" +
-        "“Tricks” are the number of rounds won for each match.\n" +
-        "During each round, each player places a card from their hand.\n" +
-        "You win a round if your card is the one with the highest value.\n" +
-        "The goal of the game is to announce a certain number of tricks and then actually winning them as well.\n" +
-        "Winning the announced amount of tricks will yield you points," +
-        " differentiating from your announcement you will get points deducted.\n" +
-        "The player with the most points at the end has won\n" +
-        "During the special round where you only have one card, you won’t \n" +
-        "be able to see your card until after everyone has announced\n" +
-        "their tricks, but you may see the cards of all the other players.\n"
-    )
+    setShowRules(!showRules)
   }
 
   const handleToggle = () => {
     setHamburgerOpen(!hamburgerOpen)
-    setShowRules(" ") //TOGGLE RULES ON AND OFF WITH SAME BUTTON!!
-    setMarginLeave("700px")
   }
 
   return (
     <nav className="navBar">
       <button onClick={handleToggle}>
         {hamburgerOpen ? (
-          <MdClose style={{ color: "#fff", width: "20px", height: "40px" }} />
+          <MdClose className="xbutton" />
         ) : (
-          <FiMenu style={{ color: "#7b7b7b", width: "20px", height: "40px" }} />
+          <FiMenu className="xbutton" />
         )}
       </button>
       <ul className={`menuNav ${hamburgerOpen ? " showMenu" : ""}`}>
-        {" "}
         <li
           onClick={clickRules}
           style={{
@@ -64,13 +63,11 @@ const HamburgerMenu = () => {
               "linear-gradient(0deg, rgba(15,113,26,1) 0%, rgba(51,173,42,1) 50%, rgba(99,255,0,1) 100%)",
           }}
         >
-          {" "}
           <ListAltIcon />
           The Rules
         </li>
-        <p>{showRules}</p>
-        <li onClick={clickLeave} style={{ marginTop: marginLeave }}>
-          {" "}
+        <Grow in={showRules}>{theRules}</Grow>
+        <li onClick={clickLeave} style={{ marginTop: "80px" }}>
           <SendIcon />
           Leave Game
         </li>
