c407a4362a35776da4b64b18ab55f764068884a8
BacLuc <lucius.bachmann@gmx.ch>
2022-04-28 14:34:54 +0200

    ScoreAnnouncing.tsx: extract component YourAnnouncedScore
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/64

--- src/components/views/game/game/scoreannouncing/ScoreAnnouncing.tsx
+++ src/components/views/game/game/scoreannouncing/ScoreAnnouncing.tsx
@@ -1,9 +1,7 @@
-import Box from "@mui/material/Box"
-import TextField from "@mui/material/TextField"
 import React, { useState } from "react"
 import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
-import insertCoin from "../../../../../img/icons/insert-coin.png"
 import { observer } from "mobx-react-lite"
+import { YourAnnouncedScore } from "./YourAnnouncedScore"
 
 export const ScoreAnnouncing = observer(() => {
   const { yourActiveHand } = useCurrentGame()
@@ -11,59 +9,13 @@ export const ScoreAnnouncing = observer(() => {
 
   const scoreAvailable = score !== null && score !== undefined
   return (
-    <Box
-      className={"score-announcing"}
-      sx={{
-        display: "flex",
-        flexDirection: { xs: "column", md: "row" },
-        alignItems: "center",
-        bgcolor: "grey.400",
-        overflow: "hidden",
-        borderRadius: "12px",
-        boxShadow: 1,
-        fontWeight: "bold",
+    <YourAnnouncedScore
+      scoreAvailable={scoreAvailable}
+      score={score}
+      onChange={(event) => {
+        setScore(parseInt(event.target.value))
       }}
-    >
-      <Box
-        component="img"
-        sx={{
-          height: 100,
-          width: 100,
-          padding: 1,
-        }}
-        alt="Announcing my scores"
-        src={insertCoin}
-      />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: { xs: "center", md: "flex-start" },
-          m: 1,
-          minWidth: { md: 180 },
-        }}
-      >
-        <Box component="span" sx={{ fontSize: 16, mt: 1 }}>
-          <TextField
-            id="outlined-number"
-            label="Number"
-            type="number"
-            style={{ width: 140 }}
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            value={scoreAvailable ? score : ""}
-            inputProps={{
-              min: 0,
-            }}
-            onChange={(event) => {
-              setScore(parseInt(event.target.value))
-            }}
-          />
-        </Box>
-      </Box>
-    </Box>
+    />
   )
 })
 
