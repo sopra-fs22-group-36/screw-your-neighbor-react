97396c83d79deffc49e3547782d6d5c5e2431630
BacLuc <lucius.bachmann@gmx.ch>
2022-04-28 18:38:13 +0200

    ScoreAnnouncing: implement modal to announce score
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/64

--- src/components/views/game/game/scoreannouncing/ScoreAnnouncing.tsx
+++ src/components/views/game/game/scoreannouncing/ScoreAnnouncing.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
 import { observer } from "mobx-react-lite"
 import { YourAnnouncedScore } from "./YourAnnouncedScore"
+import { AnnouncementModal } from "./AnnouncementModal"
 
 export const ScoreAnnouncing = observer(() => {
   const { yourActiveHand } = useCurrentGame()
@@ -9,13 +10,16 @@ export const ScoreAnnouncing = observer(() => {
 
   const scoreAvailable = score !== null && score !== undefined
   return (
-    <YourAnnouncedScore
-      scoreAvailable={scoreAvailable}
-      score={score}
-      onChange={(event) => {
-        setScore(parseInt(event.target.value))
-      }}
-    />
+    <div className={"score-announcing"}>
+      <YourAnnouncedScore
+        scoreAvailable={scoreAvailable}
+        score={score}
+        onChange={(event) => {
+          setScore(parseInt(event.target.value))
+        }}
+      />
+      <AnnouncementModal />
+    </div>
   )
 })
 
