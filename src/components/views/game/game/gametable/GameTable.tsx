e287c02d59897f13d91df3dccb86451ce193ce12
BacLuc <lucius.bachmann@gmx.ch>
2022-05-08 16:01:20 +0200

    GameTable.tsx: add indicator that round was stacked
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-server/issues/31

--- src/components/views/game/game/gametable/GameTable.tsx
+++ src/components/views/game/game/gametable/GameTable.tsx
@@ -4,11 +4,17 @@ import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
 import { observer } from "mobx-react-lite"
 import { CardComponent } from "../../../../ui/CardComponent"
 import { distributeOpponents } from "./distributeOpponents"
+import { Alert } from "@mui/material"
 import "./GameTable.scss"
 
 export const GameTable = observer(() => {
-  const { activeParticipations, myParticipation, activeRound, lastRound } =
-    useCurrentGame()
+  const {
+    activeParticipations,
+    myParticipation,
+    activeRound,
+    lastRound,
+    sortedRoundsOfActiveMatch,
+  } = useCurrentGame()
 
   const cards =
     activeRound.cards.length >= 1 ? activeRound.cards : lastRound.cards
@@ -18,9 +24,28 @@ export const GameTable = observer(() => {
     activeParticipations
   )
 
+  const reversedRounds = sortedRoundsOfActiveMatch.slice().reverse().slice(1)
+  let stackedRounds = 0
+  for (const round of reversedRounds) {
+    if (round.stacked) {
+      stackedRounds++
+    } else {
+      break
+    }
+  }
+
   return (
     <div className={"game-table"}>
       <div className={"game-table absolute-wrapper"}>
+        {stackedRounds > 0 ? (
+          <div className={"stacked-indicator"}>
+            <Alert variant={"filled"} severity={"warning"}>
+              Round was stacked, rounds in stack: {stackedRounds}
+            </Alert>
+          </div>
+        ) : (
+          <></>
+        )}
         <div className={"card-table"}>
           {cards.map((card, index) => (
             <div key={index}>
