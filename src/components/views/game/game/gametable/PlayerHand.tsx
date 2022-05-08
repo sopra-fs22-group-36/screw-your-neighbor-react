f6ec78f7dcb11a0a01d8087246500db53c336320
BacLuc <lucius.bachmann@gmx.ch>
2022-05-08 21:58:06 +0200

    PlayerHand: improve styling
    Use Player Icon instead of orange background.
    Make the cards smaller that they somehow fit there.

--- src/components/views/game/game/gametable/PlayerHand.tsx
+++ src/components/views/game/game/gametable/PlayerHand.tsx
@@ -1,5 +1,9 @@
 import React from "react"
+import { observer } from "mobx-react-lite"
 import { EntityModelParticipation } from "../../../../../generated"
+import { iriMatch } from "../../../../../util/iriMatch"
+import { useCurrentGame } from "../../../../../hooks/api/useCurrentGame"
+import { Person } from "@mui/icons-material"
 import "./PlayerHand.scss"
 import cardBack from "../../../../../img/card-backside.png"
 
@@ -8,19 +12,43 @@ export type PlayerHandProps = {
   className: string
 }
 
-export const PlayerHand = (props: PlayerHandProps) => {
+export const PlayerHand = observer((props: PlayerHandProps) => {
   if (!props.participation) {
     return <></>
   }
+  const useCurrentGameHook = useCurrentGame()
   const playerName = props.participation.player.name
+
+  const currentHand = useCurrentGameHook.activeMatch.hands
+    .filter((value) =>
+      iriMatch(props.participation._links.self, value.participation._links.self)
+    )
+    .slice(-1)[0]
+
+  const notPlayedCards = currentHand.cards.filter((card) => !card.round)
+
   return (
     <div className={`player-hand ${props.className}`}>
-      <h3>Here comes the hand of {playerName}</h3>
-      <img src={cardBack} alt="card_back_image" />
-      <img src={cardBack} alt="card_back_image" />
-      <img src={cardBack} alt="card_back_image" />
-      <img src={cardBack} alt="card_back_image" />
-      <img src={cardBack} alt="card_back_image" />
+      <div className={"content"}>
+        <div className={"opponent"}>
+          <Person
+            sx={{
+              fontSize: "13em",
+              color: "black",
+            }}
+          />
+          <span className={"player-name"}>{playerName}</span>
+        </div>
+        <div className={"cards"}>
+          {notPlayedCards.map((card) => (
+            <img
+              key={card._links.self.href}
+              src={cardBack}
+              alt="card_back_image"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
-}
+})
