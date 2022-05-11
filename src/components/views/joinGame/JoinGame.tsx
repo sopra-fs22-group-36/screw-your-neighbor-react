d8ed04987475acc7a7a8ffe19f9f4b136cfb1ea1
MorisCamporesi <moris.camporesi@uzh.ch>
2022-05-11 16:01:42 +0200

    first try on implementing the joining function, not yet working, some help needed.
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-react/issues/91

--- src/components/views/joinGame/JoinGame.tsx
+++ src/components/views/joinGame/JoinGame.tsx
@@ -1,9 +1,39 @@
 import BaseContainer from "../../ui/BaseContainer"
-import React from "react"
+import React, { useContext, useState } from "react"
 import Button from "@mui/material/Button"
 import SendIcon from "@mui/icons-material/Send"
 
+import { useGames } from "../../../hooks/api/useGames"
+import { Paths } from "../../routing/routers/Paths"
+import { useNavigate, useParams } from "react-router-dom"
+import { useApi } from "../../../hooks/api/useApi"
+import { appContext } from "../../../AppContext"
+
 const JoinGame = () => {
+  const { joinGame } = useGames()
+  const navigate = useNavigate()
+  const gameId = useParams()
+
+  const [loading, setLoading] = useState(false)
+  const { wrapApiCall, gameEntityController } = useApi()
+  const gameStore = useContext(appContext).playerStore
+
+  const findGame = async (gameId) => {
+    setLoading(true)
+    const foundGame = await wrapApiCall(
+      gameEntityController.getItemResourceGameGet("{gameId}")
+    ).finally(() => setLoading(false))
+    gameStore.setMe(foundGame)
+    return foundGame
+  }
+
+  const theGameToJoin = findGame(gameId)
+
+  const clickJoin = async () => {
+    await joinGame(await theGameToJoin)
+    navigate(Paths.GAME + `/${gameId}`)
+  }
+
   return (
     <div>
       <BaseContainer>
@@ -14,11 +44,15 @@ const JoinGame = () => {
         <h4>
           You were invited to play this game, do you want to join the game?
         </h4>
+        <Button
+          onClick={clickJoin}
+          type={"submit"}
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          TAKE ME TO THIS GAME
+        </Button>
       </BaseContainer>
-      <Button type={"submit"} variant="contained" endIcon={<SendIcon />}>
-        TAKE ME TO THIS GAME
-      </Button>
-
       <div className="background-img"></div>
     </div>
   )
