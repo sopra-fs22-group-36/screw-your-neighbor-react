752e6bd991d5dac3cda678c5ff1ab677dd6876bc
elBeato <beatus.furrer@gmail.com>
2022-05-12 23:47:38 +0200

    styling footer according mock up

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import "../../styles/ui/footer.scss"
 
-const Footer = () => {
-  return <div className="footer"></div>
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <div className="text">A webpage designed for the SoPra FS22 course</div>
+    </div>
+  )
 }
 export default Footer
