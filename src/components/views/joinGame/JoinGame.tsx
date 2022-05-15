483773a572568cf449e04c908eeacfd296bd141d
MorisCamporesi <moris.camporesi@uzh.ch>
2022-05-15 18:23:07 +0200

    wrapping in useEffect solved problems passed game by useCurrentGame set conditional view if game was found or not found validate the existence of an open lobby deleted unused code
    Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-react/issues/91

--- src/components/views/joinGame/JoinGame.tsx
+++ src/components/views/joinGame/JoinGame.tsx
@@ -1,56 +1,105 @@
 import BaseContainer from "../../ui/BaseContainer"
-import React, { useContext, useState } from "react"
+import React, { useEffect, useState } from "react"
 import Button from "@mui/material/Button"
 import SendIcon from "@mui/icons-material/Send"
 
 import { useGames } from "../../../hooks/api/useGames"
 import { Paths } from "../../routing/routers/Paths"
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { observer } from "mobx-react-lite"
 import { useCurrentGame } from "../../../hooks/api/useCurrentGame"
+import { iriMatch } from "../../../util/iriMatch"
+import { ApiError, Game } from "../../../generated"
+import { Loading } from "../../ui/Loading"
 
 const JoinGame = observer(() => {
   const { joinGame } = useGames()
   const navigate = useNavigate()
-  const gameId = useParams()
-  const { refreshGame } = useCurrentGame()
-
-  const [loading, setLoading] = useState(false)
-
-  const findgame = async () =>{
-    setLoading(true)
-    const foundGame = await refreshGame().finally(()=>setLoading(false))
-    return foundGame
+  const { refreshGame, game, id, currentGameIriFromParameter, startPollGame } =
+    useCurrentGame()
+  const [loading, setLoading] = useState(true)
+  const goBack = () => {
+    navigate(Paths.LOBBY)
   }
-  const theGame = findgame()
+
+  const gameReady =
+    game && iriMatch(currentGameIriFromParameter, game._links.self)
+
+  const catch404 = (e) => {
+    if (e instanceof ApiError) {
+      if (e.status === 404) {
+        return null
+      }
+    }
+    throw e
+  }
+
+  useEffect(() => {
+    if (!gameReady) {
+      refreshGame()
+        .catch(catch404)
+        .finally(() => setLoading(false))
+    }
+
+    const pollGameSubscription = startPollGame()
+    return () => {
+      pollGameSubscription.cancel()
+    }
+  })
 
   const clickJoin = async () => {
-    await joinGame(theGame)
-    navigate((Paths.GAME + `/${gameId}`))
+    await joinGame(game)
+    navigate(Paths.GAME + `/${id}`)
   }
 
-
   return (
-    <div>
-      <BaseContainer>
-        <h1> Welcome to "Screw your neighbour"</h1>
-      </BaseContainer>
+    <Loading ready={!loading}>
+      {(() => {
+        if (!game && !loading) {
+          return (
+            <BaseContainer>
+              <h1> Game not found </h1>
+              <Button
+                variant="contained"
+                endIcon={<SendIcon />}
+                onClick={goBack}
+              >
+                Go to lobby
+              </Button>
+            </BaseContainer>
+          )
+        }
+        if (game) {
+          const unavailableGame =
+            game.gameState !== Game.gameState.FINDING_PLAYERS
 
-      <BaseContainer>
-        <h4>
-          You were invited to play this game, do you want to join the game?
-        </h4>
-        <Button
-          //onClick={clickJoin}
-          type={"submit"}
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          TAKE ME TO THIS GAME
-        </Button>
-      </BaseContainer>
-      <div className="background-img"></div>
-    </div>
+          return (
+            <div>
+              <BaseContainer>
+                <h1> Welcome to "Screw your neighbour"</h1>
+              </BaseContainer>
+
+              <BaseContainer>
+                <h4>
+                  You were invited to play this game, do you want to join the
+                  game?
+                </h4>
+                <Button
+                  disabled={unavailableGame}
+                  onClick={clickJoin}
+                  type={"submit"}
+                  variant="contained"
+                  endIcon={<SendIcon />}
+                >
+                  TAKE ME TO THIS GAME
+                </Button>
+              </BaseContainer>
+              <div className="background-img"></div>
+            </div>
+          )
+        }
+      })()}
+    </Loading>
   )
 })
 
