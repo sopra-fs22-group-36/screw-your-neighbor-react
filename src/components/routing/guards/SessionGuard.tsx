6c465d39db1b9e413617618297bb833485fbe55f
MorisCamporesi <moris.camporesi@uzh.ch>
2022-05-15 18:41:46 +0200

    changed URL display to only display redirect if invited Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-react/issues/91

--- src/components/routing/guards/SessionGuard.tsx
+++ src/components/routing/guards/SessionGuard.tsx
@@ -24,10 +24,12 @@ export const SessionGuard = observer((props: SessionGuardProps) => {
       hasCurrentSession()
         .then((value) => {
           if (!value && location.pathname !== Paths.CREATE_PLAYER) {
-            navigate( location.pathname === '/' ? Paths.CREATE_PLAYER :
-              Paths.CREATE_PLAYER +
-                "/?redirectTo=" +
-                encodeURI(location.pathname)
+            navigate(
+              location.pathname === "/"
+                ? Paths.CREATE_PLAYER
+                : Paths.CREATE_PLAYER +
+                    "/?redirectTo=" +
+                    encodeURI(location.pathname)
             )
           }
         })
