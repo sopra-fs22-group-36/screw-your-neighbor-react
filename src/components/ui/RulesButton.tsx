24c0f23b9b77a209d161f519d5fa875394ac7c29
CarmenUZH <91155317+CarmenUZH@users.noreply.github.com>
2022-05-21 12:05:27 +0200

    Improved styling of Rules
    Readability is better now!

--- src/components/ui/RulesButton.tsx
+++ src/components/ui/RulesButton.tsx
@@ -13,22 +13,22 @@ const RulesButton = (props: { style }) => {
 
   const theRules = (
     <p>
-      {" "}
       One game is split up into nine matches where you first get decreasing
       numbers of cards each match, with a minimum of one card, and then
-      increasing numbers of cards again. (5,4,3,2,1,2,3,4). At the beginning of
-      each match, you announce a number of “Tricks”. “Tricks” are the number of
-      rounds won for each match. During each round, each player places a card
-      from their hand. You win a round if your card is the one with the highest
-      value. The goal of the game is to announce a certain number of tricks and
-      then actually winning them as well. Winning the announced amount of tricks
-      will yield you points, differentiating from your announcement you will get
-      points deducted. The player with the most points at the end has won.
+      increasing numbers of cards again. (5,4,3,2,1,2,3,4). <br /> At the
+      beginning of each match, you announce a number of “Tricks”. “Tricks” are
+      the number of rounds won for each match. During each round, each player
+      places a card from their hand. You win a round if your card is the one
+      with the highest value. The goal of the game is to announce a certain
+      number of tricks and then actually winning them as well. Winning the
+      announced amount of tricks will yield you points, differentiating from
+      your announcement you will get points deducted. <br /> The player with the
+      most points at the end has won. <br />
       During the special round where you only have one card, you won’t be able
       to see your card until after everyone has announced their tricks, but you
-      may see the cards of all the other players. - (If you are ever confused on
-      what to do you can also just hover over the buttons/cards to get
-      information) - Have Fun! :)
+      may see the cards of all the other players. <br /> If you are ever
+      confused on what to do you can also just hover over the buttons/cards to
+      get more information <br /> <big>Have Fun! :)</big>
     </p>
   )
 
