f72645b1a250fd687d99b8ab02c6c80a71b43404
MorisCamporesi <moris.camporesi@uzh.ch>
2022-05-23 13:47:36 +0200

    changes gameSummary view, and the winners podium added variable styling for gameWin / gameLoss Issue: https://github.com/sopra-fs22-group-36/screw-your-neighbor-react/issues/13

--- src/components/views/game/gamesummary/GameSummary.tsx
+++ src/components/views/game/gamesummary/GameSummary.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { observer } from "mobx-react-lite"
 import { useGames } from "../../../../hooks/api/useGames"
 import { extractId } from "../../../../util/extractId"
-import BaseContainer from "../../../ui/BaseContainer"
 import SendIcon from "@mui/icons-material/Send"
 import LogoutIcon from "@mui/icons-material/Logout"
 import Button from "@mui/material/Button"
@@ -11,7 +10,7 @@ import { Paths } from "../../../routing/routers/Paths"
 import ReactConfetti from "react-confetti"
 import { WinnersPodium } from "./WinnersPodium"
 import "../gamesummary/GameSummary.scss"
-import Box from "@mui/material/Box";
+import Box from "@mui/material/Box"
 
 export const GameSummary = observer(() => {
   const navigate = useNavigate()
@@ -31,30 +30,38 @@ export const GameSummary = observer(() => {
       <ReactConfetti gravity={0.01} opacity={0.5} tweenDuration={5000} />
       <div className="gameSummary-box">
         <WinnersPodium />
-        <div className="gameSummary-buttons">
-          <Box p={1} m={1}>
-            <Button
-              style={{ minWidth: "250px", minHeight: "50px", borderRadius: 20}}
-              disabled={loading}
-              variant="contained"
-              endIcon={<LogoutIcon />}
-              onClick={clickCloseGame}
-            >
-              Close game
-            </Button>
-          </Box>
-          <Box p={1} m={1}>
-            <Button
-              style={{ minWidth: "250px", minHeight: "60px", borderRadius: 20}}
-              disabled={loading}
-              variant="contained"
-              endIcon={<SendIcon />}
-              onClick={clickStartNewGame}
-            >
-              Start new Game
-            </Button>
-          </Box>
-        </div>
+        <Box className="gameSummary-buttonBox">
+          <Button
+            style={{
+              minWidth: "200px",
+              minHeight: "40px",
+              borderRadius: 20,
+              margin: "10px",
+            }}
+            color={"success"}
+            disabled={loading}
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={clickStartNewGame}
+          >
+            Start new Game
+          </Button>
+          <Button
+            style={{
+              minWidth: "200px",
+              minHeight: "40px",
+              borderRadius: 20,
+              margin: "10px",
+            }}
+            color={"error"}
+            disabled={loading}
+            variant="contained"
+            endIcon={<LogoutIcon />}
+            onClick={clickCloseGame}
+          >
+            Close game
+          </Button>
+        </Box>
       </div>
     </div>
   )
