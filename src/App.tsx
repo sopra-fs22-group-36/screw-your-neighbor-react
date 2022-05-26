b7283b52ba77cfc55b81997f0acad24ebd091331
BacLuc <lucius.bachmann@gmx.ch>
2022-05-26 16:26:42 +0200

    App.tsx: replace ugly browser scrollbar with darkScrollBar() from material ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import AppRouter from "./components/routing/routers/AppRouter"
 import { AppContextProvider } from "./AppContext"
 import { ApiErrorSnackBar } from "./components/ui/ApiErrorSnackBar"
 import { HttpsGuard } from "./components/routing/guards/HttpsGuard"
+import { darkScrollbar, GlobalStyles } from "@mui/material"
 
 const App = () => {
   return (
     <div>
+      <GlobalStyles
+        styles={{
+          ...darkScrollbar(),
+        }}
+      />
       <HttpsGuard>
         <AppContextProvider>
           <ApiErrorSnackBar />
