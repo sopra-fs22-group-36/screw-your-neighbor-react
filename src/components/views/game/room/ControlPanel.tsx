afd04757b33c06900f0632bbf9f0fc0b9728dba0
CarmenUZH <91155317+CarmenUZH@users.noreply.github.com>
2022-05-27 15:14:09 +0200

    Leave button in room

--- src/components/views/game/room/ControlPanel.tsx
+++ src/components/views/game/room/ControlPanel.tsx
@@ -1,22 +1,13 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
 import { useCurrentGame } from "../../../../hooks/api/useCurrentGame"
-import { Paths } from "../../../routing/routers/Paths"
-import Button from "@mui/material/Button"
 import Box from "@mui/material/Box"
-import LogoutIcon from "@mui/icons-material/Logout"
 import { Tooltip } from "@mui/material"
 import StartButton from "../../../ui/StartButton"
+import LeaveButton from "../../../ui/LeaveButton"
 
 export const ControlPanel = () => {
-  const navigate = useNavigate()
-  const { loading, leaveGame, activeParticipations } = useCurrentGame()
+  const { activeParticipations } = useCurrentGame()
   const shouldDisable = activeParticipations.length < 2
-
-  const clickLeave = async () => {
-    await leaveGame()
-    navigate(Paths.LOBBY)
-  }
 
   return (
     <div className={"control-panel"}>
@@ -43,15 +34,14 @@ export const ControlPanel = () => {
       </Tooltip>
 
       <Box p={1} m={2}>
-        <Button
-          style={{ minWidth: "250px", minHeight: "60px", borderRadius: 20 }}
-          disabled={loading}
-          variant="contained"
-          endIcon={<LogoutIcon />}
-          onClick={clickLeave}
-        >
-          Leave Room
-        </Button>
+        <LeaveButton
+          style={{
+            minWidth: "250px",
+            minHeight: "60px",
+            borderRadius: 15,
+            color: "white",
+          }}
+        />
       </Box>
     </div>
   )
