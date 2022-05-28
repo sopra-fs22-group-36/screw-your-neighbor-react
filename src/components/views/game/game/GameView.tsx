e7af50fce8646125bdcc9abb939ef83a5a44fb4c
BacLuc <lucius.bachmann@gmx.ch>
2022-05-28 15:46:25 +0200

    GameView: add little box with number of won tricks and announces score

--- src/components/views/game/game/GameView.tsx
+++ src/components/views/game/game/GameView.tsx
@@ -3,11 +3,12 @@ import { observer } from "mobx-react-lite"
 import { GameTable } from "./gametable/GameTable"
 import { VideoChat } from "./videochat/VideoChat"
 import { ScoreBoard } from "./scoreboard/ScoreBoard"
-import { Grid } from "@mui/material"
+import { Grid, Tooltip } from "@mui/material"
 import { YourHand } from "./yourhand/YourHand"
 import "./GameView.scss"
 import HamburgerMenu from "../../../ui/HamburgerMenu"
 import { AnnouncementModal } from "./scoreannouncing/AnnouncementModal"
+import { useCurrentGame } from "../../../../hooks/api/useCurrentGame"
 
 /**
  * The main game page inside the Jass-Stube includes a table with all players
@@ -15,6 +16,11 @@ import { AnnouncementModal } from "./scoreannouncing/AnnouncementModal"
  * @returns
  */
 const GameView = observer(() => {
+  const { yourActiveHand } = useCurrentGame()
+
+  const textPart1 = `You have won ${yourActiveHand?.numberOfWonTricks} tricks`
+  const textPart2 = `and announced that you will make ${yourActiveHand?.announcedScore} tricks.`
+  const pointsOverViewToolTipText = `${textPart1}, ${textPart2}`
   return (
     <div className="game div-box">
       <Grid container spacing={0} className={"top-row"}>
@@ -35,6 +41,14 @@ const GameView = observer(() => {
             <GameTable />
             <YourHand />
             <AnnouncementModal />
+            <div className={"points-overview"}>
+              <Tooltip title={pointsOverViewToolTipText} arrow={true}>
+                <span>
+                  {yourActiveHand?.numberOfWonTricks}/
+                  {yourActiveHand?.announcedScore}
+                </span>
+              </Tooltip>
+            </div>
           </div>
         </Grid>
       </Grid>
